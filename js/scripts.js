1c2abdc29fde0b310903bb5c30c83029ba0bfdc9
Jiwon Han <jiwon1.han@gmail.com>
2020-01-14 20:40:26 -0800

    walnus picture slide up and slide down

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".clickable").click(function(){
+    $("#walrus-showing").slideDown();
+
+
+  $(".clickable2").click(function() {
+    $("#walurs-hidden").slideUp();
+    $("#walrus-showing").slideUp();
+
+  });
+});
+
+});
